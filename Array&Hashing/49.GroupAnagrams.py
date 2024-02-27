3e4bdc76f1b74a08a0715cf495df932f9512329b
Klesse <pedro_klesse@hotmail.com>
2024-02-27 09:57:58 -0300

    fix: final solution to 49 group anagrams

--- Array&Hashing/49.GroupAnagrams.py
+++ Array&Hashing/49.GroupAnagrams.py
@@ -14,3 +14,10 @@ def groupAnagrams(self, strs):
         result.append(aux)
         anams = anagrams.copy()
     return result
+
+
+# Hashmap O (m * n * 26)
+# m = number os strings
+# n = number of words in each string, that will be counted (tea -> 1 a, 1 e, 1 t)
+# 26 = all letters in alphabet
+
