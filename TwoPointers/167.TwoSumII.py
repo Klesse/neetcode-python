3792adf0f34464c4041f329967d1581eea38c18a
Klesse <pedro_klesse@hotmail.com>
2024-03-10 19:43:51 -0300

    feat: 167 done

---
+++ TwoPointers/167.TwoSumII.py
@@ -0,0 +1,12 @@
+# First solution
+
+def twoSum(numbers, target):
+    i, j = 0, len(numbers)-1
+
+    while i < j:
+        if numbers[i] + numbers[j] == target:
+            return [i+1,j+1]
+        if numbers[i] + numbers[j] > target:
+            j-=1
+        else:
+            i+=1
